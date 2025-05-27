f77a4421cfee6419ade8a7db497538f3312fa1e6
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-27 14:22:12 +0900

    Create output.asm

---
+++ output.asm
@@ -0,0 +1,22 @@
+[ORG 0x7C00]
+[BITS 16]
+	mov si, str_literal_0
+	call print
+
+print:
+    lodsb
+    or al, al
+    jz print_done
+    mov ah, 0x0E
+    int 0x10
+    jmp print
+print_done:
+    mov ah, 0x0E
+    int 0x10
+    ret
+
+	str_literal_0 db "Hello", 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
+
