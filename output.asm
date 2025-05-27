d435bfc3bc80b7a0a943ad97f1bac4cbd0421c42
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-27 14:43:24 +0900

    Update output.asm

--- output.asm
+++ output.asm
@@ -2,6 +2,8 @@
 [BITS 16]
 	mov si, str_literal_0
 	call print
+	mov si, str_literal_1
+	call print
 	jmp $
 
 print:
@@ -12,9 +14,14 @@ print:
     int 0x10
     jmp print
 print_done:
+    mov al, 0x0D
+    int 0x10
+    mov al, 0x0A
+    int 0x10
     ret
 
-str_literal_0 db "Hello", 0
+str_literal_0 db "Hello, world!", 0
+str_literal_1 db "from Native Basic", 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
